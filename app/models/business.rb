ffcf9d7d0d1f686fc98359bd3f3c1829727f37c0
Dave Nelson <davidicus21@gmail.com>
2014-07-03 15:58:42 -0600

    Created devise user model

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,2 +1,4 @@
 class Business < ActiveRecord::Base
+  has_many :users
+  has_many :trucks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   root 'site#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
