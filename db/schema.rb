0ea39ccf66c4dd4087cb355679eb858aa73aee55
Dave Nelson <davidicus21@gmail.com>
2014-07-03 22:03:25 -0600

    added first_name, last_name, and phone to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703214829) do
+ActiveRecord::Schema.define(version: 20140704035236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 20140703214829) do
     t.integer  "business_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name",                          null: false
+    t.string   "last_name",                           null: false
+    t.string   "phone"
   end
 
   add_index "users", ["business_id"], name: "index_users_on_business_id", using: :btree
