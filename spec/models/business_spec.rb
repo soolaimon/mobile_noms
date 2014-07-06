2c24f0f45bda28a2a5705622acdf6baf47d8a6e8
Dave Nelson <davidicus21@gmail.com>
2014-07-06 10:27:52 -0600

    added validation tests for user

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Business, :type => :model do
-  describe "associations", :focus do
+  describe "associations" do
     it { should have_many :users}
     it { should have_many :trucks}
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe User, :type => :model do
   describe "associations" do
     it { should belong_to :business }
   end
+
+  describe "validations", :focus do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+  end
 end
