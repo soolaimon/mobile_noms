fd7ff0370e017b1069de76da98bc434f0899a8ae
Dave Nelson <davidicus21@gmail.com>
2014-07-06 12:18:31 -0600

    did factory for truck (just name, food type, description)

--- spec/factories/trucks.rb
+++ spec/factories/trucks.rb
@@ -2,5 +2,16 @@
 
 FactoryGirl.define do
   factory :truck do
+    name { Faker::Company.name }
+    food_type { Faker::BaconIpsum.word}
+    description { Faker::BaconIpsum.paragraph }
+    business
   end
 end
+
+    # t.string   "name",        null: false
+    # t.string   "food_type"
+    # t.text     "description"
+    # t.integer  "business_id"
+    # t.datetime "starts_at"
+    # t.datetime "ends_at"
