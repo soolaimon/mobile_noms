397c3fbd636ce9517adccf28c3ff577fb6a268c2
Dave Nelson <davidicus21@gmail.com>
2014-07-06 22:00:33 -0600

    started factory for user, then stopped. i don't owe you an explanation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,3 +4,24 @@ FactoryGirl.define do
   factory :user do
   end
 end
+
+ # t.string   "email",                  default: "", null: false
+ #    t.string   "encrypted_password",     default: "", null: false
+ #    t.string   "reset_password_token"
+ #    t.datetime "reset_password_sent_at"
+ #    t.datetime "remember_created_at"
+ #    t.integer  "sign_in_count",          default: 0,  null: false
+ #    t.datetime "current_sign_in_at"
+ #    t.datetime "last_sign_in_at"
+ #    t.string   "current_sign_in_ip"
+ #    t.string   "last_sign_in_ip"
+ #    t.string   "confirmation_token"
+ #    t.datetime "confirmed_at"
+ #    t.datetime "confirmation_sent_at"
+ #    t.string   "unconfirmed_email"
+ #    t.integer  "business_id"
+ #    t.datetime "created_at"
+ #    t.datetime "updated_at"
+ #    t.string   "first_name",                          null: false
+ #    t.string   "last_name",                           null: false
+ #    t.string   "phone"
