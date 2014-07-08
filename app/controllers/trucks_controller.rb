143789efe26d0f78fec337da867e3c69c271c824
Rubiromi <hiromi.deyoung@gmail.com>
2014-07-07 22:01:18 -0600

    Add a test for Elasticsearch

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,3 +1,9 @@
 class TrucksController < ApplicationController
-
+  def index
+    Truck.reindex
+    trucks = Truck.search "cup"
+    trucks.each do |truck|
+      puts product.name
+    end
+  end
 end
