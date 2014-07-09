063f35b1282aff5019eea4a091ad78b6f784eeec
justindruiz <justinruizwrestling@gmail.com>
2014-07-09 16:11:17 -0600

    finished file upload capability with carrierwave

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,8 +1,9 @@
 class Truck < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
+
   belongs_to :user
   validates :name, presence: true
 
   searchkick
-
-  mount_uploader :image, ImageUploader
+  
 end
