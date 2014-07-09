446fe112a72c967958f7fe449a57e7d7865315c9
Rubiromi <hiromi.deyoung@gmail.com>
2014-07-09 13:52:59 -0600

    create truck in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+trucks = Truck.create([{
+  name:            'Super Taco',
+  food_type:       'Happy Mexican Food',
+  description:     'Best Mexican food to make people high',
+  starts_at:       '2014-07-09 10:00:00',
+  ends_at:         '2014-07-09 2:30:00',
+  created_at:      '2014-07-09 03:44:14',
+  updated_at:      '2014-07-09 03:44:14',
+  user_id:         2,
+  image:           nil
+  }])
+
+trucks = Truck.create([{
+  name:            'Super Taco',
+  food_type:       'Happy Mexican Food',
+  description:     'Best Mexican food to make people high',
+  starts_at:       '2014-07-09 10:00:00',
+  ends_at:         '2014-07-09 2:30:00',
+  created_at:      '2014-07-09 03:44:14',
+  updated_at:      '2014-07-09 03:44:14',
+  user_id:         2,
+  image:           nil
+  }])
