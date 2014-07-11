b404061d4cb2cd3db82f36dc7d4b423ccc58f8a5
Dave Nelson <davidicus21@gmail.com>
2014-07-11 15:06:51 -0600

    added tests for location

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#associations" do
+    it {should belong_to :truck}
+  end
+
+  describe "#validations" do
+    it {should validate_presence_of :truck_id}
+  end
+
+  describe "#address" do
+    before do
+      @location = create(:location, street_address: "813 melrose terrace",
+       street_address2: "apt 23", city: "Newport News", state: "Virginia", zip: "23608")
+    end
+    it "should return 813 melrose terrace apt 23, Newport News, Virginia 23608" do
+      expect(@location.address).to eq "813 melrose terrace apt 23, Newport News, Virginia 23608" 
+    end
+  end
 end
