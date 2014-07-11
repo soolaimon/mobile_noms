ff76d3bafce59a243619adedebaa283cb47fa7ae
Dave Nelson <davidicus21@gmail.com>
2014-07-11 14:37:26 -0600

    scoped locations within truck on routes, truck has_one location

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   root 'site#index'
 
-  resources :trucks
+  resources :trucks do
+    resources :locations
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
