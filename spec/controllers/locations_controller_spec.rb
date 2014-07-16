e15785d9795924468296353a8bf8e2945c379ffe
Dave Nelson <davidicus21@gmail.com>
2014-07-16 16:53:57 -0600

    tested location show and update

--- spec/controllers/locations_controller_spec.rb
+++ spec/controllers/locations_controller_spec.rb
@@ -2,8 +2,20 @@ require 'rails_helper'
 
 RSpec.describe LocationsController, :type => :controller do
 
+  describe "#show" do
+    before do
+      @location = create(:location)
+    end
+    it "should show render the json for the location I selected", :focus do
+      get :show, truck_id: @location.truck.id, id: @location.id, format: :json
+      expect(assigns(:location).id).to eq @location.id    
+      expect(response).to render_template('show')
+    end
+  end
+
   describe "#edit" do
     before do
+      expect(response).to be_success 
       @location = create(:location)
     end
 
@@ -15,19 +27,17 @@ RSpec.describe LocationsController, :type => :controller do
     end
   end
 
-  describe "#update", :focus do
+  describe "#update" do
     before do
       @location = create(:location)
     end
 
     context "when saving properly" do
       it "should change the value of the locations latitude to 30.2 and longitude to 40.222" do
-        put :update, id: @location.id, truck_id: @location.truck.id, location: {latitude: 30.2, longitude: 40.222}
-        expect(@location.reload.latitude).to eq 30.2
-        expect(@location.reload.longitude).to eq 40.222
+        put :update, id: @location.id, truck_id: @location.truck.id, location: {latitude: 30.1223286, longitude: 40.185467}
+        expect(@location.reload.latitude).to eq 30.1223286
+        expect(@location.reload.longitude).to eq 40.185467
       end
     end
   end
-
-
 end
