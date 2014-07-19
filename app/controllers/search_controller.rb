6ac5171408f92f1172b5d068ea765bae3825be3f
Dave Nelson <davidicus21@gmail.com>
2014-07-19 10:20:53 -0600

    fixed what i broke

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,7 @@ class SearchController < ApplicationController
 
   def index
     @text_results = Truck.search(params[:q])
-    trucks_in_range(@text_results).compact
-
+    @results = trucks_in_range(@text_results).compact
   end
 
   private
@@ -11,5 +10,4 @@ class SearchController < ApplicationController
   def trucks_in_range(trucks)
     trucks.collect {|truck| truck if truck.location.distance_from([session[:latitude], session[:longitude]]) <= params[:distance].to_f}
   end
-
 end
