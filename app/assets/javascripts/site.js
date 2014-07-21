c6da935fd7fa87dec6f6c89c8660e0d7dc51123a
Dave Nelson <davidicus21@gmail.com>
2014-07-21 13:59:14 -0600

    merging hiromi's changes

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,3 +1,24 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function() {
+
+  fullMap();
+  
+});
+
+var fullMap = function () {
+
+  var $cityTrucksMap = $('#city-trucks-map');
+  var url = "/site.json" ;
+  if ($cityTrucksMap.length) {
+    $.getJSON(url, function (data) {
+      console.log(data);
+    });
+
+  }
+
+
+      
+};
--- app/controllers/concerns/searching.rb
+++ app/controllers/concerns/searching.rb
@@ -1,6 +1,6 @@
 module Searching
-  
-  private
+  extend ActiveSupport::Concern
+
   def trucks_in_range(trucks, distance)
     trucks.collect {|truck| truck if truck.location.distance_from([session[:latitude], session[:longitude]]) <= distance.to_f}
   end
