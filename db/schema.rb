19d0fe56593d889fad7e372b30c91e9175d47d0a
Rubiromi <hiromi.deyoung@gmail.com>
2014-07-23 13:38:50 -0600

    fixing merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20140722225149) do
 
-# These are extensions that must be enabled in order to support this database
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "frequent_locations", force: true do |t|
