e50d0f4ef8a19be7d0b0ee706b61d1c05a97313e
Dave Nelson <davidicus21@gmail.com>
2014-07-23 16:58:42 -0600

    testing session data

--- app/controllers/concerns/searching.rb
+++ app/controllers/concerns/searching.rb
@@ -2,6 +2,8 @@ module Searching
   extend ActiveSupport::Concern
 
   def trucks_in_range(trucks, distance)
+    puts "IN SEARCHING***********************#{session[:latitude]}"
+    puts "IN SEARCHING***********************#{session[:longitude]}"
     trucks.collect {|truck| truck if truck.location.distance_from([session[:latitude], session[:longitude]]) <= distance.to_f}
   end
 end
