984a027b08143e2b6498538352dd1eb76ffeb385
Dave Nelson <davidicus21@gmail.com>
2014-07-23 14:01:52 -0600

    rmed region from carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,6 @@ CarrierWave.configure do |config|
     :provider => 'AWS',
     :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region => ENV['S3_REGION']
   }
   # For testing, upload files to local 'tmp' folder.
   if Rails.env.test? || Rails.env.development?
