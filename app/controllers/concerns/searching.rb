ec52da704817f7af37772464f60f8ade4d545e8b
Dave Nelson <davidicus21@gmail.com>
2014-07-23 17:28:15 -0600

    fixed deployment probs. locations in heroku db had nil values for lat-lng.

--- app/controllers/concerns/searching.rb
+++ app/controllers/concerns/searching.rb
@@ -2,11 +2,6 @@ module Searching
   extend ActiveSupport::Concern
 
   def trucks_in_range(trucks, distance)
-    puts "IN SEARCHING***********************#{session[:latitude]}"
-    puts "IN SEARCHING***********************#{session[:longitude]}"
-    puts "IN SEARCHING***********************#{distance}"
-    puts "IN SEARCHING***********************#{distance.to_f}"
-
     trucks.collect {|truck| truck if truck.location.distance_from([session[:latitude], session[:longitude]]) <= distance.to_f}
   end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,9 +4,6 @@ class SiteController < ApplicationController
   def index
     @title = 'Home'
     trucks = Truck.all
-    puts "***************#{session[:latitude]}"
-    puts "***************#{session[:longitude]}"
-
     if session[:latitude] != nil && session[:longitude] != nil
       @trucks = trucks_in_range(trucks, 20).compact
     end
