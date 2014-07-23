128089f48633f1bfb3661a2e6b595dd3a1f54f0c
Dave Nelson <davidicus21@gmail.com>
2014-07-23 16:51:15 -0600

    800th time's a charm?

--- app/controllers/concerns/searching.rb
+++ app/controllers/concerns/searching.rb
@@ -2,8 +2,6 @@ module Searching
   extend ActiveSupport::Concern
 
   def trucks_in_range(trucks, distance)
-    if session[:latitude] && session[:longitude]
-      trucks.collect {|truck| truck if truck.location.distance_from([session[:latitude], session[:longitude]]) <= distance.to_f}
-    end
+    trucks.collect {|truck| truck if truck.location.distance_from([session[:latitude], session[:longitude]]) <= distance.to_f}
   end
 end
