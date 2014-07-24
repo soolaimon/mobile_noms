acb27f1202a42cb1df870caa77354e07a68125ef
Dave Nelson <davidicus21@gmail.com>
2014-07-23 21:50:10 -0600

    added region back to carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,6 +3,7 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS',
     :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :region => ENV['S3_REGION'],
     :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
   }
   # For testing, upload files to local 'tmp' folder.
