b3a1be419ead991ca28694fb62d3831a763a2032
Dave Nelson <davidicus21@gmail.com>
2014-07-25 11:58:59 -0600

    added website url as attribute of truck, put it on form and show page

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -53,7 +53,7 @@ class TrucksController < ApplicationController
   def truck_params
 
     params.require(:truck).permit(:name, :food_type, :user_id, :description, :monday_open, :monday_close, :tuesday_open, :tuesday_close, :wednesday_open, :wednesday_close, :thursday_open, :thursday_close, :friday_open, :friday_close, :saturday_open, :saturday_close, :sunday_open, :sunday_close,
-     :cash, :visa, :discover, :mastercard, :american_express, :image, :twitter_handle)
+     :cash, :visa, :discover, :mastercard, :american_express, :image, :twitter_handle, :website)
 
   end
 
